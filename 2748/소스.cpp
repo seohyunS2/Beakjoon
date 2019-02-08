4cf20dbd0dcbf2b836d4d1d26ef0e133a6d12d28
seohyunS2 <45092652+seohyunS2@users.noreply.github.com>
2019-02-08 23:49:55 +0900

    피보나치(long long)
    Complete !

---
+++ 2748/소스.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<stdio.h>
+#include <algorithm>
+using namespace std;
+#define MAX_SIZE 5000002
+long long arr[MAX_SIZE];
+
+int main()
+{
+	int n;
+	arr[0] = 0;
+	arr[1] = 1;
+	for (int i = 2; i < 100; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+	cin >> n;
+	cout << arr[n];
+}
